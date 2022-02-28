d3cbc7ba009a4a01d91fc32c72a430c5de76c557
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-02-28 22:56:00 +0530

    based on std namespace, variables and comments

---
+++ tut3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+//This is a single line comment.
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
---
+++ tut4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+/*This 
+is
+ 
+    a
+     multi 
+     line 
+     comment.*/
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
