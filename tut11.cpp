af0a6a55ca99e1174499983b2b2ffc37ded76e43
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-01 08:16:35 +0530

    C++ basic programms

---
+++ tut11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 14, b = 15;
+    float pi = 3.14;
+    char c = 'u';
+    cout<<"This is tutorial 11. \nHere the value of a and b is "<<a<<b;
+    cout<<"\nThe value of pi is "<<pi;
+    cout<<"\nThe character to be printed is "<<c;
+    return 0;
+}
