0a0f64d1142829ac55a2d514a0200783f7ba70cf
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-04 14:20:51 +0530

    If else-if ladder in c++
    selection control structure

---
+++ tut27.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{   // Selection Control Structure: If else-if else ladder
+   
+    int age;
+    cout<< "Tell me your age"<<endl;
+    cin>>age;
+
+    if((age<18) && (age>0))
+    {
+        cout<<"You can not come to my party"<<endl;
+    }
+    else if(age==18)
+    {
+        cout<<"You are a kid and you will get a kid pass to the party"<<endl;
+    }
+    else if(age<1)
+    {
+        cout<<"You are not yet born"<<endl;
+    }
+    else
+    {
+        cout<<"You can come to my party";
+    }
+
+    return 0;
+}
