fbcc1e64ca2296fdcab7b7c52653711725afdfb3
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-04 09:07:47 +0530

    operator precedence in c++

--- tut24.cpp
+++ tut24.cpp
@@ -1,19 +1,14 @@
 #include<iostream>
-#include<iomanip>
+
 using namespace std;
+
 int main()
 {
-    int a = 34;
-    cout<<"The value of a was :"<<a;
-    a = 45;
-    cout<<"The value of a is :"<<a;
+    // Operator Precedence 
+    // precedence : which operator is used to be resolve first
+    int a = 3, b = 4;
+    int c = ((((a * 5) + b) - 45) + 87);
+    cout<<c;
     
-    // Constants in c++
-    // constants will permanently fix the value given to any varriable and due to this compiler will show the error if the same varriable is introduced with different value within the same source code.
-    // const int a = 3;
-    // cout<<"The value of a was: "<<a<<endl;
-    //a = 45; // you will get error because a is a constant
-    //cout<<"The value of a is: "<<a<endl;
-
     return 0;
 }
