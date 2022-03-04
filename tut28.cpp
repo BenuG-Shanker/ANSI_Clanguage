ccaa447a987e544b9f8ab9afe9611d3f426f50cb
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-04 14:21:24 +0530

    Switch statement with break
    Selection Control Structure

---
+++ tut28.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // Selection Control Structure: Switch Case Statement with working of break statement  
+
+    int age;
+    cout<< "Tell me your age"<<endl;
+    cin>>age;
+
+    switch (age)
+    {
+    case 18:
+        cout<<"You are 18"<<endl;
+        break;
+    case 22:
+        cout<<"You are 22"<<endl;
+        break;
+    case 2:
+        cout<<"You are 2"<<endl;
+        break;
+
+    default:
+        cout<<"No special cases"<<endl;
+        break;
+    }
+
+    return 0;
+}
