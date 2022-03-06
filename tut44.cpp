78327665594c171519f6d8dee24ddc91fd823a4d
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-06 13:44:09 +0530

    Arrays in C++

---
+++ tut44.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int mathMarks[4];
+    mathMarks[0] = 2278;
+    mathMarks[1] = 7278;
+    mathMarks[2] = 3278;
+    mathMarks[3] = 578;
+    
+    cout<<"These are math marks"<<endl;
+    cout<<mathMarks[0]<<endl;
+    cout<<mathMarks[1]<<endl;
+    cout<<mathMarks[2]<<endl;
+    cout<<mathMarks[3]<<endl;
+    return 0;
+}
