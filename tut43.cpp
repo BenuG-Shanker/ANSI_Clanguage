93c2e6cd0611043ef6d7e7d193da54afe6f02fba
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-06 13:43:49 +0530

    Arrays in C++

---
+++ tut43.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int marks[4] = {23, 45, 56, 89};
+    
+    cout<<"These are marks"<<endl;
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    return 0;
+}
