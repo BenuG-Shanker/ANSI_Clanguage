d3d45de08181dee7d71b7599dd47a215fbdfb7c1
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-07 07:48:47 +0530

    concept regarding "const" keyword in C++

---
+++ tut69.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+// int strlen(const char *p)
+// {
+//     //used when not to change the value feeded in the formal argument within functions.
+//     //Ans, significant mostly while using referenced variable and pointers.
+// }
+int main() 
+{
+    
+    return 0;
+}
