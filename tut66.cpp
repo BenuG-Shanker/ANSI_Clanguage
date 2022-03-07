3d5043202a5182a8a214ca170e799c6e01e6587c
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-03-07 07:46:20 +0530

    concept of Inline Functions in C++

---
+++ tut66.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+inline int product(int a, int b)
+{
+    return a * b;
+}
+//Inline function: compiler will replace the function call by the actual function code using Inline funciton..
+//Also, used to reduce the overhead cost of the whole program.
+int main() 
+{
+    int a, b;
+    cout<<"Enter the value of a and b";
+    cin>>a>>b;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+    cout<<"The product of a and b is "<<product(a, b)<<endl;
+
+    return 0;
+}
