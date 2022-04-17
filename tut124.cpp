e87a52b42965da31420b1549f6321a2ebc9fa365
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-04-17 07:52:57 +0530

    detail about Polymorphism in C++

---
+++ tut124.cpp
@@ -0,0 +1,21 @@
+/* 
+Polymorphism
+many   forms --> one thing having multiple forms(one name and multiple forms)
+example: function overloading, operator overloading, virtual functions
+Types of polymorphism:-
+1. Compile Time Polymorphism -->
+    > acheive in two ways.
+    > object is bound to function call during compile time.
+    > it is also called as early bindin, static binding.
+    > acheived by two ways
+        :- functional overloading 
+        :- operator overloading.
+2. Run Time Polymorphism -->
+    > trickier than first one.
+    > achived by one way 
+        :- Virtual Functions.
+    > compiler will postpone(or defer) the decision of the linking of the function call in the run time.
+    > it will not bind as compile time does.
+
+
+*/
