0637afaa254ea795aee6a7f0f68ca7d16c580471
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-04-17 07:54:29 +0530

    Introduction to Virtual Functions in polymorphism in C++

---
+++ tut126.cpp
@@ -0,0 +1,39 @@
+// virtual functions
+#include<iostream>
+
+using namespace std;
+
+class BaseClass
+{
+    public:
+        int var_base = 1;
+        virtual void display()// if virtual is not written then display of base class will run, otherwise derived class display will run.
+        {
+            cout<<"1 Displaying Base Class variable var_base "<<var_base<<endl;
+        }
+};
+
+class DerivedClass : public BaseClass
+{
+    public:
+        int var_derived = 2;
+        void display()
+        {
+            cout<<"2 Displaying Base Class variable var_base "<<var_base<<endl;
+            cout<<"2 Displaying Derived Class variable var_derived "<<var_derived<<endl;
+        }
+};
+
+int main()
+{
+    BaseClass * base_class_pointer; 
+
+    BaseClass obj_base;
+    DerivedClass obj_derived;
+
+    base_class_pointer = &obj_derived;
+    base_class_pointer->display();// default behaviour
+
+// virtual function -> overwriting of a default behaviour
+    return 0;
+}
