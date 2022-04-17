355671961a149b5a21a2e6b505b4b92a3e57ac13
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-04-17 07:52:32 +0530

    concept of this pointer in C++

---
+++ tut123.cpp
@@ -0,0 +1,26 @@
+// this pointer
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    int a;
+    public:
+        A & setData(int a)
+        {
+            this->a = a;
+            return *this;
+        }
+        void getData(void)
+        {
+            cout<<"The value of a is "<<a<<endl;
+        }
+};
+int main()
+{
+    A a;
+    a.setData(4).getData();
+    // setData() is returning an object where we can attach getData() implicitly to get the result.
+    return 0;
+}
