90505e64f24a5d7849ab8ee6174074ec942d99c7
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2022-04-18 09:00:40 +0530

    Basic theory about working with file digramatically in C++

---
+++ tut130.cpp
@@ -0,0 +1,26 @@
+// working with files in C++
+// file is a pattern of data which is stored in a disk
+/*
+                                        +------------------+
+                                        |     Files        |
+                                        +------------------+
+                                               /\   |
+                                                |   |   [can read the file]
+                                                |   |   
+                       [can write to the file]  |   |
+                                                |   |
+                                                |  \/
+                                +-----------------------------------+
+                                |             C++ Program           |
+                                +-----------------------------------+
+                                        |                  /\
+                                        |                   |           
+[cout>>-can print screen using cout]    |                   |   [cin>>-can be used to input]
+                                        |                   |
+                                        |                   |
+                                       \/                   |
+                                +------------------+    +------------------+
+                                |  Console Output  |    |  Console Input   |
+                                |  Screen/Monitor  |    |  Keyboard        |
+                                +------------------+    +------------------+                             
+*/
